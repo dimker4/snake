acf2289ad1906d57e65a5dac6cc5413a2cbd3bb3
dimker <dimker4@gmail.com>
2018-11-19 22:02:42 +0500

    proc way

---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int y = 3;
+            char c = '*';
+
+            Draw(x, y, c);
+
+            Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
+        }
+    }
+}

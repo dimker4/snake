f8ecab8b34434cd276d11ef41d5d710c3fe6eac4
dimker <dimker4@gmail.com>
2018-11-20 20:21:43 +0500

    oop way

---
+++ Snake/Snake/HorizontalLine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorizontalLine
+    {
+        List<Point> pList;
+
+        public HorizontalLine (int xLeft, int xRight, int y, char c)
+        {
+            pList = new List<Point>();
+
+            for (var x = xLeft; x <= xRight; x++)
+            {
+                var p = new Point(x, y, c);
+                pList.Add(p);
+            }
+        }
+
+        public void Draw ()
+        {
+            foreach (var i in pList)
+            {
+                i.Draw();
+            }
+        }
+    }
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,8 +10,17 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p = new Point(1, 3, '*');
-            p.Draw();
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
+            Point p2 = new Point(5, 7, '@');
+            p2.Draw();
+
+            HorizontalLine topLine = new HorizontalLine(0, 80, 0, '-');
+            HorizontalLine botLine = new HorizontalLine(0, 80, 20, '-');
+
+            topLine.Draw();
+            botLine.Draw();
 
             Console.ReadLine();
         }
